518e9b2e89bb862d8af1dd6d7f08ab5cb7aec195
Minho Kang <busster@naver.com>
2026-03-10 18:52:43 +0900

    예시용 코드 주석 처리

--- src/api/shopApi.jsx
+++ src/api/shopApi.jsx
@@ -14,16 +14,16 @@ export const register = async(formData) =>{
     return res.data;
 }
 
+//예시용 코드
+// export const getOne = async(no)=>{
+//     const res = await axios.get(`${prefix}/${no}`)
+//     return res.data
+// }
 
-export const getOne = async(no)=>{
-    const res = await axios.get(`${prefix}/${no}`)
-    return res.data
-}
+// export const getList = async (pageParam) =>{
 
-export const getList = async (pageParam) =>{
-
-    const {page,size} = pageParam
-    const res = await axios.get(`${prefix}/list`,{params:{page:page,size:size}})
-    return res.data
-}
+//     const {page,size} = pageParam
+//     const res = await axios.get(`${prefix}/list`,{params:{page:page,size:size}})
+//     return res.data
+// }
 
