c516bf75e348a44f184a09dd47d48cde05644ab7
johanness917 <jhhan917@gmail.com>
2026-03-16 14:22:42 +0900

    authApi 임시 수정

--- src/api/authApi.jsx
+++ src/api/authApi.jsx
@@ -1,19 +1,21 @@
 import apiClient from "./apiClient";
 
+export const API_SERVER_HOST = 'http://localhost:8080'
+
 //로그인, 회원가입 등을 처리하는 api
 
 const prefix = "/api/auth";
 
 //로그인 api 호출 함수
-export const login = async(formData) =>{
-    const res = await apiClient.post(`${prefix}/login`,formData)
-    return res.data;
+export const login = async (formData) => {
+  const res = await apiClient.post(`${prefix}/login`, formData)
+  return res.data;
 }
 
 //회원가입 처리 함수
-export const register = async(formData) =>{
-    const res = await apiClient.post(`${prefix}/register`,formData)
-    return res.data;
+export const register = async (formData) => {
+  const res = await apiClient.post(`${prefix}/register`, formData)
+  return res.data;
 }
 
 //권한을 받아오는 함수
