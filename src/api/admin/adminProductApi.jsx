5659aa28feb9731154853c9e65b36ec5996bbf27
Kim HyunMin <khm2938@gmail.com>
2026-03-20 19:02:07 +0900

    관리자 상품 등록 페이지 구현

--- src/api/admin/adminProductApi.jsx
+++ src/api/admin/adminProductApi.jsx
@@ -2,6 +2,7 @@ import apiClient from "../apiClient";
 
 const prefix = "/api/admin/product";
 
+// 관리자 상품 목록 조회
 export const getAdminProductList = async (searchParams) => {
   const response = await apiClient.get(prefix, {
     params: searchParams,
@@ -9,6 +10,15 @@ export const getAdminProductList = async (searchParams) => {
 
   console.log("getAdminProductList 요청 params:", searchParams);
   console.log("getAdminProductList 응답:", response.data);
+
+  return response.data;
+};
+
+// 관리자 상품 추가
+export const postAdminProductAdd = async (formData) => {
+  const response = await apiClient.post(prefix, formData);
+
+  console.log("postAdminProductAdd 응답:", response.data);
 
   return response.data;
 };
