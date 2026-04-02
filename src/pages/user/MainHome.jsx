8a4ba8cabd992c54337831609f5d16dfce59e19f
siyul3330 <siyul3330@gmail.com>
2026-04-02 16:55:00 +0900

    리뷰, 최근 본 상품 삭제

--- src/pages/user/MainHome.jsx
+++ src/pages/user/MainHome.jsx
@@ -6,8 +6,7 @@ import MainProductList from "../../components/user/MainProductList";
 import Footer from "../../components/user/Footer";
 import HomeProductSection from "../../components/user/HomeProductSection";
 import PopularKeywords from "../../components/user/PopularKeywords";
-import RecentlyViewed from "../../components/user/RecentlyViewed";
-import HomeReviewSection from "../../components/user/HomeReviewSection";
+
 import { getHomeMainData } from "../../api/user/productApi";
 import { useUser } from "../../context/UserContext";
 // ★ 룰렛 모달 import 추가
@@ -32,7 +31,6 @@ const MainHome = () => {
     bestProducts: [],
     saleProducts: [],
     recommendProducts: [],
-    recentReviews: [],
     popularKeywords: [],
   });
   const [loading, setLoading] = useState(false);
@@ -136,9 +134,7 @@ const MainHome = () => {
             moreLink="/?sort=new"
           />
 
-          <HomeReviewSection reviews={homeData.recentReviews} />
 
-          <RecentlyViewed />
         </>
       )}
 
