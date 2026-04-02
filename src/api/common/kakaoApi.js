9178a7552d898a3bb48e0cbcddba41a34cbb9d4d
Minho Kang <busster@naver.com>
2026-04-02 12:20:09 +0900

    카카오 로그인 리다이렉트 uri 변경

--- src/api/common/kakaoApi.js
+++ src/api/common/kakaoApi.js
@@ -1,6 +1,6 @@
 const REST_API_KEY = "d5130702ba4c3c0f55cc0ff03e5925f0";
 // const REDIRECT_URI = "http://localhost:5173/member/kakao";
-const REDIRECT_URI = "https://shop-frontend-topaz.vercel.app/member/kakao";
+const REDIRECT_URI = "https://khshop.best/member/kakao";
 
 export const getKakaoLoginLink = () => {
   return `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
